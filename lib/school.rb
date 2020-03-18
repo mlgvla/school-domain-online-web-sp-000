726a00cb35b9ca05567fb9e3b9d8721d027e030a
Monica Gerard <mlgerardvla@gmail.com>
2020-03-18 00:04:48 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,6 +6,15 @@ class School
   end
 
   def add_student(name, grade)
-
+    #if key already exists, shovel on to the array the key points to
+    #else create the key and the name it points to
+    if @roster.has_key?(grade)
+      @roster[grade] << name
+    else
+      @roster[grade] = []
+      @roster[grade] << name
+    end
+    
+    
   end
 end
